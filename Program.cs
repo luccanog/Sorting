d4743a46f3bf69678836c370ba3d59b7be610008
luccanog <luccathomaz@gmail.com>
2024-04-03 14:39:26 -0300

    adding quick sort

--- Program.cs
+++ Program.cs
@@ -20,93 +20,117 @@ namespace Sorting
                 _ => throw new ArgumentException("Invalid algorithm name"),
             };
 
-            algorithm.Sort(nums);
+            var result = algorithm.Sort(nums);
+            PrintResult(result);
         }
 
-    private static int GetSortingAlgOption()
-    {
-        int[] currentValidOptions = new int[] { 1, 2 };
-        int input;
-        do
+        private static int GetSortingAlgOption()
         {
-            WriteLine(ConsoleColor.White, "\n\n# Choose the desired sorting algorithm: ");
-            WriteLine(ConsoleColor.White, "\n1 - Merge sort \n2 - Quick sort");
-            Write(ConsoleColor.White, "\nR: ");
-            input = Convert.ToInt32(Console.ReadLine().ToString());
-
-            if (!currentValidOptions.Contains(input))
+            int[] currentValidOptions = new int[] { 1, 2 };
+            int input;
+            do
             {
-                WriteLine(ConsoleColor.Red, "Error: algorithm option not valid.");
-            }
-        } while (!currentValidOptions.Contains(input));
+                WriteLine(ConsoleColor.White, "\n\n# Choose the desired sorting algorithm: ");
+                WriteLine(ConsoleColor.White, "\n1 - Merge sort \n2 - Quick sort");
+                Write(ConsoleColor.White, "\nR: ");
+                input = Convert.ToInt32(Console.ReadLine().ToString());
 
-        return input;
-    }
-
-    private static int[] GetInputArray()
-    {
-        var nums = new List<int>();
-        do
-        {
-            Write(ConsoleColor.White, "\n# Please insert the int32 array to be sorted(comma-separated): ");
-            var input = Console.ReadLine().Split(',');
-
-            foreach (var str in input)
-            {
-
-                var success = Int32.TryParse(str, out var num);
-                if (success)
+                if (!currentValidOptions.Contains(input))
                 {
-                    nums.Add(num);
+                    WriteLine(ConsoleColor.Red, "Error: algorithm option not valid.");
+                }
+            } while (!currentValidOptions.Contains(input));
+
+            return input;
+        }
+
+        private static int[] GetInputArray()
+        {
+            var nums = new List<int>();
+            do
+            {
+                Write(ConsoleColor.White, "\n# Please insert the int32 array to be sorted(comma-separated): ");
+                var input = Console.ReadLine().Split(',');
+
+                foreach (var str in input)
+                {
+
+                    var success = Int32.TryParse(str, out var num);
+                    if (success)
+                    {
+                        nums.Add(num);
+                    }
+                }
+
+                if (!nums.Any())
+                {
+                    WriteLine(ConsoleColor.Red, "Error: input not valid.");
+                }
+
+            } while (!nums.Any());
+
+            PrintInput(nums);
+
+            return nums.ToArray();
+        }
+
+        private static void PrintInput(List<int> input)
+        {
+            Write(ConsoleColor.Gray, "Parsed input array: ");
+
+            var sb = new StringBuilder("[");
+            for (int i = 0; i < input.Count; i++)
+            {
+                sb.Append(input[i]);
+                if (i != input.Count - 1)
+                {
+                    sb.Append(", ");
+                }
+                else
+                {
+
+                    sb.Append("]");
                 }
             }
 
-            if (!nums.Any())
-            {
-                WriteLine(ConsoleColor.Red, "Error: input not valid.");
-            }
-
-        } while (!nums.Any());
-
-        PrintInput(nums);
-
-        return nums.ToArray();
-    }
-
-    private static void PrintInput(List<int> input)
-    {
-        Write(ConsoleColor.Gray, "Parsed input array: ");
-
-        var sb = new StringBuilder("[");
-        for (int i = 0; i < input.Count; i++)
-        {
-            sb.Append(input[i]);
-            if (i != input.Count - 1)
-            {
-                sb.Append(", ");
-            }
-            else
-            {
-
-                sb.Append("]");
-            }
+            Write(ConsoleColor.Gray, sb.ToString());
         }
 
-        Write(ConsoleColor.Gray, sb.ToString());
-    }
+        private static void PrintResult(int[] result)
+        {
+            WriteLine(ConsoleColor.Green, "\n# Done ! ! !");
+            Write(ConsoleColor.White, "Sorted array: ");
 
-    private static void WriteLine(ConsoleColor color, string message)
-    {
-        Console.ForegroundColor = color;
-        Console.WriteLine(message);
-        Console.ResetColor();
-    }
+            var sb = new StringBuilder("[");
+            for (int i = 0; i < result.Length; i++)
+            {
+                sb.Append(result[i]);
+                if (i != result.Length - 1)
+                {
+                    sb.Append(", ");
+                }
+                else
+                {
 
-    private static void Write(ConsoleColor color, string message)
-    {
-        Console.ForegroundColor = color;
-        Console.Write(message);
-        Console.ResetColor();
+                    sb.Append("]");
+                }
+            }
+
+            Write(ConsoleColor.White, sb.ToString());
+        }
+
+        private static void WriteLine(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        private static void Write(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            Console.ResetColor();
+        }
     }
-}
 }
--- SortingAlgorithm.cs
+++ SortingAlgorithm.cs
@@ -17,7 +17,43 @@
     {
         public override int[] Sort(int[] nums)
         {
-            throw new NotImplementedException();
+            if (nums.Length <= 1)
+            {
+                return nums;
+            }
+
+            if (nums.Length == 2)
+            {
+                if (nums[0] > nums[1])
+                {
+                    return new int[2] { nums[1], nums[0] };
+                }
+
+                return nums;
+            }
+
+            var pivot = nums[0];
+            var lower = new List<int>();
+            var greater = new List<int>();
+
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                var num = nums[i];
+                if (num > pivot)
+                {
+                    greater.Add(num);
+                }
+                else
+                {
+                    lower.Add(num);
+                }
+            }
+
+            var lowerSorted = Sort(lower.ToArray());
+            var greaterSorted = Sort(greater.ToArray());
+
+            return lowerSorted.Append(pivot).Concat(greaterSorted).ToArray();
         }
     }
 }
