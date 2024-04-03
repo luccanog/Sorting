d2ee181dd00904aaf9997b38a660699d6e6bfa4f
luccanog <luccathomaz@gmail.com>
2024-04-02 21:25:35 -0300

    adding abstract class

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection.Metadata.Ecma335;
-using System.Text;
+﻿using System.Text;
 
 namespace Sorting
 {
@@ -12,93 +11,102 @@ namespace Sorting
 
             var nums = GetInputArray();
 
-            var alg = GetSortingAlgOption();
+            var option = GetSortingAlgOption();
+
+            SortingAlgorithm algorithm = option switch
+            {
+                1 => new MergeSort(),
+                2 => new QuickSort(),
+                _ => throw new ArgumentException("Invalid algorithm name"),
+            };
+
+            algorithm.Sort(nums);
         }
 
-        private static int GetSortingAlgOption()
+    private static int GetSortingAlgOption()
+    {
+        int[] currentValidOptions = new int[] { 1, 2 };
+        int input;
+        do
         {
-            int[] currentValidOptions = new int[] { 1, 2 };
-            int input;
-            do
+            WriteLine(ConsoleColor.White, "\n\n# Choose the desired sorting algorithm: ");
+            WriteLine(ConsoleColor.White, "\n1 - Merge sort \n2 - Quick sort");
+            Write(ConsoleColor.White, "\nR: ");
+            input = Convert.ToInt32(Console.ReadLine().ToString());
+
+            if (!currentValidOptions.Contains(input))
             {
-                WriteLine(ConsoleColor.White, "\n\n# Choose the desired sorting algorithm: ");
-                WriteLine(ConsoleColor.White, "\n1 - Merge sort \n2 - Quick sort");
-                Write(ConsoleColor.White, "\nR: ");
-                input = Convert.ToInt32(Console.ReadLine().ToString());
+                WriteLine(ConsoleColor.Red, "Error: algorithm option not valid.");
+            }
+        } while (!currentValidOptions.Contains(input));
 
-                if (!currentValidOptions.Contains(input))
-                {
-                    WriteLine(ConsoleColor.Red, "Error: algorithm option not valid.");
-                }
-            } while (!currentValidOptions.Contains(input));
+        return input;
+    }
 
-            return input;
-        }
-
-        private static int[] GetInputArray()
+    private static int[] GetInputArray()
+    {
+        var nums = new List<int>();
+        do
         {
-            var nums = new List<int>();
-            do
+            Write(ConsoleColor.White, "\n# Please insert the int32 array to be sorted(comma-separated): ");
+            var input = Console.ReadLine().Split(',');
+
+            foreach (var str in input)
             {
-                Write(ConsoleColor.White, "\n# Please insert the int32 array to be sorted(comma-separated): ");
-                var input = Console.ReadLine().Split(',');
 
-                foreach (var str in input)
+                var success = Int32.TryParse(str, out var num);
+                if (success)
                 {
-
-                    var success = Int32.TryParse(str, out var num);
-                    if (success)
-                    {
-                        nums.Add(num);
-                    }
-                }
-
-                if (!nums.Any())
-                {
-                    WriteLine(ConsoleColor.Red, "Error: input not valid.");
-                }
-
-            } while (!nums.Any());
-
-            PrintInput(nums);
-
-            return nums.ToArray();
-        }
-
-        private static void PrintInput(List<int> input)
-        {
-            Write(ConsoleColor.Gray, "Parsed input array: ");
-
-            var sb = new StringBuilder("[");
-            for (int i = 0; i < input.Count; i++)
-            {
-                sb.Append(input[i]);
-                if (i != input.Count - 1)
-                {
-                    sb.Append(", ");
-                }
-                else
-                {
-
-                    sb.Append("]");
+                    nums.Add(num);
                 }
             }
 
-            Write(ConsoleColor.Gray, sb.ToString());
-        }
+            if (!nums.Any())
+            {
+                WriteLine(ConsoleColor.Red, "Error: input not valid.");
+            }
 
-        private static void WriteLine(ConsoleColor color, string message)
-        {
-            Console.ForegroundColor = color;
-            Console.WriteLine(message);
-            Console.ResetColor();
-        }
+        } while (!nums.Any());
 
-        private static void Write(ConsoleColor color, string message)
-        {
-            Console.ForegroundColor = color;
-            Console.Write(message);
-            Console.ResetColor();
-        }
+        PrintInput(nums);
+
+        return nums.ToArray();
     }
+
+    private static void PrintInput(List<int> input)
+    {
+        Write(ConsoleColor.Gray, "Parsed input array: ");
+
+        var sb = new StringBuilder("[");
+        for (int i = 0; i < input.Count; i++)
+        {
+            sb.Append(input[i]);
+            if (i != input.Count - 1)
+            {
+                sb.Append(", ");
+            }
+            else
+            {
+
+                sb.Append("]");
+            }
+        }
+
+        Write(ConsoleColor.Gray, sb.ToString());
+    }
+
+    private static void WriteLine(ConsoleColor color, string message)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+    private static void Write(ConsoleColor color, string message)
+    {
+        Console.ForegroundColor = color;
+        Console.Write(message);
+        Console.ResetColor();
+    }
+}
 }
---
+++ SortingAlgorithm.cs
@@ -0,0 +1,23 @@
+﻿namespace Sorting
+{
+    public abstract class SortingAlgorithm
+    {
+        public abstract int[] Sort(int[] nums);
+    }
+
+    public class MergeSort : SortingAlgorithm
+    {
+        public override int[] Sort(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class QuickSort : SortingAlgorithm
+    {
+        public override int[] Sort(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
