565b4ce397c388b003a1dc5b4f5f27192d286d99
luccanog <luccathomaz@gmail.com>
2024-04-10 16:09:46 -0300

    adding different test scenarios

--- Tests/SortingAlgorithmTests.cs
+++ Tests/SortingAlgorithmTests.cs
@@ -2,15 +2,18 @@ namespace Sorting.UnitTests
 {
     public class SortingAlgorithmTests
     {
-        [Fact]
-        public void QuickSort_ShouldSucceed()
+        [Theory]
+        [InlineData(new[] { 4, 3, 2 }, new[] { 2, 3, 4 })]
+        [InlineData(new[] { 100, 50, 25 }, new[] { 25, 50, 100 })]
+        [InlineData(new[] { 3, 5, 2, 1, 4 }, new[] { 1, 2, 3, 4, 5 })]
+        public void QuickSort_ShouldSucceed(int[] input, int[] expectedResult)
         {
-            var arr = new[] { 3, 5, 2, 1, 4 };
             var alg = new QuickSort();
 
-            var result = alg.Sort(arr);
+            var result = alg.Sort(input);
 
-            Assert.Equal(new[] {1,2,3,4,5}, result);
+            Assert.Equal(expectedResult, result);
         }
+
     }
 }
